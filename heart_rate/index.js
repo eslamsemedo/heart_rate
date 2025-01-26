5c798a418ee9c18469c158f86fc127adc4f78f16
eslam2semedo <eslamsemedo@gmail.com>
2025-01-26 23:50:49 +0200

    start

---
+++ heart_rate/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/eslam", (req, res) => {
+    res.send("eslam")
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+});
