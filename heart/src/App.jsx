96a77f603187000d8d8fb2a3df602d30fb8651fa
eslam2semedo <eslamsemedo@gmail.com>
2025-01-27 05:22:10 +0200

    edit

---
+++ heart/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p class="text-blue-600 dark:text-sky-400">The quick brown fox...</p>
+    </>
+  )
+}
+
+export default App
